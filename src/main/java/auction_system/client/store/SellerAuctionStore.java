6a0ddd6c186bb4ca5ad5c21d63ccbac2e9cfbc1f
52ducbanh <52ducbanh@gmail.com>
2026-05-21 18:33:41 +0700

    refactor and merge code of client common server in order to push in main and fix some bugs of client and server.

---
+++ src/main/java/auction_system/client/store/SellerAuctionStore.java
@@ -0,0 +1,49 @@
+package auction_system.client.store;
+
+import auction_system.common.dto.AuctionDTO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class SellerAuctionStore {
+    private static SellerAuctionStore instance;
+    private final ObservableList<AuctionDTO> auctions = FXCollections.observableArrayList();
+
+    private SellerAuctionStore() {
+    }
+
+    public static synchronized SellerAuctionStore getInstance() {
+        if (instance == null) {
+            instance = new SellerAuctionStore();
+        }
+        return instance;
+    }
+
+    public ObservableList<AuctionDTO> getAuctions() {
+        return auctions;
+    }
+
+    public void setAuctions(java.util.List<AuctionDTO> newAuctions) {
+        auctions.setAll(newAuctions);
+    }
+
+    public void addAuction(AuctionDTO auction) {
+        auctions.add(auction);
+    }
+
+    public void updateAuction(AuctionDTO auction) {
+        int index = -1;
+        for (int i = 0; i < auctions.size(); i++) {
+            if (auctions.get(i).getId() == auction.getId()) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            auctions.set(index, auction);
+        }
+    }
+
+    public void removeAuction(int auctionId) {
+        auctions.removeIf(auction -> auction.getId() == auctionId);
+    }
+}
